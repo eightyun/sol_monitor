678a81637a2b864e8289fe1c36297d4fb9c5eb88
eightclound <879950144@qq.com>
2024-10-19 18:02:56 +0800

    perf:加锁以优化了输出

--- src/rocksdb_handler.cpp
+++ src/rocksdb_handler.cpp
@@ -3,36 +3,62 @@
 #include <iostream>
 #include <cstdint>
 #include <nlohmann/json.hpp>
+#include <mutex>
 
-rocksdb::DB * db ;
+rocksdb::DB * db = nullptr ;
 rocksdb::Options options ;
+extern std::mutex mtx; 
 
 void InitRocksDB()
 {
+    if (db != nullptr) 
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        std::cerr << "RocksDB is already initialized" << std::endl;
+        return;
+    }
+
     options.create_if_missing = true ;
     rocksdb::Status status = rocksdb::DB::Open(options , "/tmp/testdb" , &db) ;
-    if (!status.ok()) std::cerr << "Unable to open RocksDB" << std::endl;
+    if (!status.ok())
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        std::cerr << "Unable to open RocksDB: " << status.ToString() << std::endl;
+    }
 }
 
-void StoreBlockData(uint64_t slot , const std::string & blockdata)
+void StoreBlockData(const std::string & key , const std::string & blockdata)
 {
-    rocksdb::Status status = db->Put(rocksdb::WriteOptions() , std::to_string(slot) , blockdata);
-    if(!status.ok()) std::cerr << "Failed to store block data" << std::endl;
+    rocksdb::Status status = db->Put(rocksdb::WriteOptions() , key , blockdata);
+    if(!status.ok())
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+         std::cerr << "Failed to store block data: " << status.ToString() << std::endl;
+    }
 }
 
-nlohmann::json GetStoredBlockData(uint64_t slot)
+nlohmann::json GetStoredBlockData(const uint64_t & slot)
 {
     std::string value ;
     rocksdb::Status status = db->Get(rocksdb::ReadOptions() , std::to_string(slot) , &value);
     if(!status.ok()) 
     {
-        std::cerr << "Failed to get block data" << std::endl;
+        std::lock_guard<std::mutex> lock(mtx);
+        std::cerr << "Failed to get block data" << status.ToString() << std::endl;
         return nlohmann::json();  
     }
-    
+
     if (!value.empty()) 
     {
-        return nlohmann::json::parse(value);
+        try 
+        {
+            return nlohmann::json::parse(value);
+        } 
+        catch (const nlohmann::json::parse_error & e) 
+        {
+            std::lock_guard<std::mutex> lock(mtx);
+            std::cerr << "JSON parse error: " << e.what() << std::endl;
+        }
     }
 
     return nlohmann::json();  
@@ -40,6 +66,10 @@ nlohmann::json GetStoredBlockData(uint64_t slot)
 
 void CloseRocksDB()
 {
-    delete db ;
+    if (db) 
+    {
+        delete db;
+        db = nullptr;
+    }
 }
 
